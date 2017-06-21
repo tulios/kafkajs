5c4b956a089255aac0843fa80b2e81f35829f3fb
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-06-21 20:52:55 +0200

    Force log to output a single line

--- src/loggers/console.js
+++ src/loggers/console.js
@@ -8,19 +8,19 @@ const LEVELS = {
 
 const createLevel = (label, level, currentLevel, loggerFunction) => (message, extra = {}) => {
   if (level > currentLevel) return
-  loggerFunction(
-    Object.assign(
-      {
-        level: label,
-        timestamp: new Date().toISOString(),
-        message,
-      },
-      extra
-    )
+  const logData = Object.assign(
+    {
+      level: label,
+      timestamp: new Date().toISOString(),
+      message,
+    },
+    extra
   )
+
+  loggerFunction(JSON.stringify(logData))
 }
 
-const createLogger = ({ level = LEVELS.INFO } = {}) => ({
+const createLogger = ({ level = parseInt(process.env.LOG_LEVEL, 10) || LEVELS.INFO } = {}) => ({
   info: createLevel('INFO', LEVELS.INFO, level, console.info),
   error: createLevel('ERROR', LEVELS.ERROR, level, console.error),
   warn: createLevel('WARN', LEVELS.WARN, level, console.warn),
