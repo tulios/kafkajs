35ad11c229f09795e484bb2ba7c776c5deed214d
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-11-17 02:43:49 +0100

    Refactor OffsetManager/initializeConsumerOffsets

--- src/consumer/offsetManager/initializeConsumerOffsets.js
+++ src/consumer/offsetManager/initializeConsumerOffsets.js
@@ -1,22 +1,22 @@
 const Long = require('long')
+const { keys, assign } = Object
+
+const indexPartitions = (obj, { partition, offset }) => assign(obj, { [partition]: offset })
+const shouldInitializeOffset = offset => Long.fromValue(offset).equals(-1)
 
 module.exports = (consumerOffsets, topicOffsets) => {
-  const indexPartitions = (obj, { partition, offset }) =>
-    Object.assign(obj, { [partition]: offset })
-
   const indexTopics = (obj, { topic, partitions }) =>
-    Object.assign(obj, { [topic]: partitions.reduce(indexPartitions, {}) })
-
-  const shouldInitializeOffset = offset => Long.fromValue(offset).equals(-1)
+    assign(obj, { [topic]: partitions.reduce(indexPartitions, {}) })
 
   const indexedConsumerOffsets = consumerOffsets.reduce(indexTopics, {})
   const indexedTopicOffsets = topicOffsets.reduce(indexTopics, {})
 
-  return Object.keys(indexedConsumerOffsets).map(topic => {
+  return keys(indexedConsumerOffsets).map(topic => {
+    const partitions = indexedConsumerOffsets[topic]
     return {
       topic,
-      partitions: Object.keys(indexedConsumerOffsets[topic]).map(partition => {
-        const offset = indexedConsumerOffsets[topic][partition]
+      partitions: keys(partitions).map(partition => {
+        const offset = partitions[partition]
         const resolvedOffset = shouldInitializeOffset(offset)
           ? indexedTopicOffsets[topic][partition]
           : offset
