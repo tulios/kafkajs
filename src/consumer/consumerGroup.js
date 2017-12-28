a04e05608f3285b8dedae19a1cd91bfd2d1634db
Tulio Ornelas <ornelas.tulio@gmail.com>
2017-12-28 18:49:52 +0100

    Expose seek method to consumer API
    The seek method exposed to the consumer API only schedules the offset seek, the operation will be performed in the fetch loop to prevent inconsistencies

--- src/consumer/consumerGroup.js
+++ src/consumer/consumerGroup.js
@@ -40,6 +40,7 @@ module.exports = class ConsumerGroup {
     this.maxBytes = maxBytes
     this.maxWaitTime = maxWaitTimeInMs
 
+    this.seekOffset = []
     this.coordinator = null
     this.generationId = null
     this.leaderId = null
@@ -119,6 +120,21 @@ module.exports = class ConsumerGroup {
     this.offsetManager.resolveOffset({ topic, partition, offset })
   }
 
+  /**
+   * Update the consumer offset for the given topic/partition. This will be used
+   * on the next fetch. If this API is invoked for the same topic/partition more
+   * than once, the latest offset will be used on the next fetch.
+   *
+   * @param {string} topic
+   * @param {number} partition
+   * @param {string} offset
+   */
+  seek({ topic, partition, offset }) {
+    const sameTopicPartition = entry => entry.topic === topic && entry.partition === partition
+    this.seekOffset = this.seekOffset.filter(entry => !sameTopicPartition(entry))
+    this.seekOffset.push({ topic, partition, offset })
+  }
+
   async commitOffsets() {
     await this.offsetManager.commitOffsets()
   }
@@ -143,6 +159,17 @@ module.exports = class ConsumerGroup {
     try {
       const { topics, maxBytesPerPartition, maxWaitTime, minBytes, maxBytes } = this
       const requestsPerLeader = {}
+
+      while (this.seekOffset.length > 0) {
+        const seekEntry = this.seekOffset.pop()
+        this.logger.debug('Seek offset', {
+          groupId: this.groupId,
+          memberId: this.memberId,
+          seek: seekEntry,
+        })
+        await this.offsetManager.seek(seekEntry)
+      }
+
       await this.offsetManager.resolveOffsets()
 
       for (let topic of topics) {
@@ -197,17 +224,7 @@ module.exports = class ConsumerGroup {
       }
 
       if (e.name === 'KafkaJSOffsetOutOfRange') {
-        this.logger.error('Offset out of range, resetting to default offset', {
-          topic: e.topic,
-          groupId: this.groupId,
-          memberId: this.memberId,
-          partition: e.partition,
-        })
-
-        await this.offsetManager.setDefaultOffset({
-          topic: e.topic,
-          partition: e.partition,
-        })
+        await this.recoverFromOffsetOutOfRange(e)
       }
 
       if (e.name === 'KafkaJSBrokerNotFound') {
@@ -216,5 +233,19 @@ module.exports = class ConsumerGroup {
 
       throw e
     }
+  }
+
+  async recoverFromOffsetOutOfRange(e) {
+    this.logger.error('Offset out of range, resetting to default offset', {
+      topic: e.topic,
+      partition: e.partition,
+      groupId: this.groupId,
+      memberId: this.memberId,
+    })
+
+    await this.offsetManager.setDefaultOffset({
+      topic: e.topic,
+      partition: e.partition,
+    })
   }
 }
