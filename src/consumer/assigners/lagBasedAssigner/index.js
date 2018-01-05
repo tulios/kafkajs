9447497adb3546ab28f5aed8950966dc6430b149
Tommy Brunn <tommy.brunn@gmail.com>
2018-01-05 15:21:39 +0100

    Create lag based assigner
    Attempts to assign partitions in such a way that offset is evenly
    distributed across members, while retaining a balance in number of
    partitions assigned.

---
+++ src/consumer/assigners/lagBasedAssigner/index.js
@@ -0,0 +1,136 @@
+const { assign, values, keys } = Object
+
+const head = (arr = []) => arr[0]
+
+const initializeAssignments = members =>
+  members.reduce((assignments, memberId) => assign({}, { [memberId]: {} }, assignments), {})
+
+const byOffsetLagDescending = ({ offsetLag: a }, { offsetLag: b }) => b - a
+
+const mergePartitionOffsets = (consumerOffsets, latestOffsets) => {
+  return consumerOffsets.map(({ topic, partitions: consumerOffsetForPartititions }) => {
+    const { partitions: latestOffsetsByPartition } = latestOffsets.find(
+      ({ topic: latestOffsetsTopic }) => topic === latestOffsetsTopic
+    )
+
+    return {
+      topic,
+      partitions: consumerOffsetForPartititions.map(
+        ({ partition: consumerOffsetPartition, offset: startingOffset }) => ({
+          partition: consumerOffsetPartition,
+          offsetLag:
+            parseInt(
+              latestOffsetsByPartition.find(
+                ({ partition: latestOffsetForPartition }) =>
+                  latestOffsetForPartition === consumerOffsetPartition
+              ).offset
+            ) - parseInt(startingOffset),
+        })
+      ),
+    }
+  })
+}
+
+/**
+ * LagBasedAssigner
+ * 
+ * Assigns partitions to members based on offset lag - attempting to spread
+ * it as evenly as possible.
+ * 
+ * The difference in number of assigned partitions between the most assigned
+ * member and the least assigned member should never be greater than 1.
+ * 
+ * If the two members with the fewest number of partitions assigned have the same
+ * number of assigned partitions, the one where the sum of the offset lag of the
+ * assigned partitions is the lowest should be assigned the unassigned partition.
+ * 
+ * @param {Cluster} cluster
+ * @param {string} groupId
+ * @returns {function}
+ */
+module.exports = ({ cluster, groupId, logger }) => {
+  const calculateOffsetLag = async topics => {
+    const topicsWithPartitions = topics.map(topic => ({
+      topic,
+      partitions: cluster
+        .findTopicPartitionMetadata(topic)
+        .map(({ partitionId: partition }) => ({ partition })),
+    }))
+
+    const latestOffsets = await cluster.fetchTopicsOffset(topicsWithPartitions)
+    const coordinator = await cluster.findGroupCoordinator({ groupId })
+    const { responses: consumerOffsets } = await coordinator.offsetFetch({
+      groupId,
+      topics: topicsWithPartitions,
+    })
+
+    const offsetLagByPartition = mergePartitionOffsets(consumerOffsets, latestOffsets)
+
+    return offsetLagByPartition
+  }
+
+  /**
+   * @param {array} members array of members, e.g:
+                                [{ memberId: 'test-5f93f5a3' }]
+   * @param {array} topics
+   * @returns {array} object partitions per topic per member, e.g:
+   *                   [
+   *                     {
+   *                       memberId: 'test-5f93f5a3',
+   *                       memberAssignment: {
+   *                         'topic-A': [0, 2, 4, 6],
+   *                         'topic-B': [0, 2],
+   *                       },
+   *                     },
+   *                     {
+   *                       memberId: 'test-3d3d5341',
+   *                       memberAssignment: {
+   *                         'topic-A': [1, 3, 5],
+   *                         'topic-B': [1],
+   *                       },
+   *                     }
+   *                   ]
+   */
+  return async ({ members, topics }) => {
+    const sortedMembers = members.map(({ memberId }) => memberId).sort()
+    const assignments = initializeAssignments(sortedMembers)
+
+    const assignMember = (memberId, topic, partitionId) => {
+      if (!assignments[memberId][topic]) {
+        assignments[memberId][topic] = []
+      }
+      assignments[memberId][topic].push(partitionId)
+    }
+
+    const assignedPartitions = memberId =>
+      values(assignments[memberId] || {}).reduce(
+        (assignedPartitions, partitionsForTopic) => assignedPartitions.concat(partitionsForTopic),
+        []
+      )
+
+    const leastAssignedMember = members => {
+      const numberOfAssignedPartitions = memberId => assignedPartitions(memberId).length
+      const byNumberOfAssignedPartitions = (a, b) =>
+        numberOfAssignedPartitions(a) - numberOfAssignedPartitions(b)
+
+      return head(members.sort(byNumberOfAssignedPartitions))
+    }
+
+    const topicPartitionOffsetLag = await calculateOffsetLag(topics)
+
+    topics.forEach(topic => {
+      const partitions = topicPartitionOffsetLag
+        .find(({ topic: topicWithLag }) => topicWithLag === topic)
+        .partitions.sort(byOffsetLagDescending)
+
+      partitions.forEach(({ partition, offsetLag }) =>
+        assignMember(leastAssignedMember(sortedMembers), topic, partition)
+      )
+    })
+
+    return keys(assignments).map(memberId => ({
+      memberId,
+      memberAssignment: assignments[memberId],
+    }))
+  }
+}
---
+++ src/consumer/assigners/lagBasedAssigner/index.spec.js
@@ -0,0 +1,93 @@
+const createLagBasedAssigner = require('./index')
+const { newLogger } = require('testHelpers')
+
+describe('Consumer > assigners > LagBasedAssigner', async () => {
+  let cluster, topics, metadata, assignPartitions
+
+  beforeEach(() => {
+    metadata = {}
+    cluster = {
+      findTopicPartitionMetadata: topic => metadata[topic],
+      fetchTopicsOffset: async topics =>
+        topics.filter(({ topic }) => metadata[topic]).map(({ topic, partitions }) => ({
+          topic,
+          partitions: partitions
+            .filter(({ partition }) =>
+              metadata[topic].map(({ partitionId }) => partitionId).includes(partition)
+            )
+            .map(({ partition }) => ({
+              partition,
+              offset: `${parseInt(partition) * 2}`,
+            })),
+        })),
+      findGroupCoordinator: async () => ({
+        offsetFetch: async ({ topics }) => ({
+          responses: topics.filter(({ topic }) => metadata[topic]).map(({ topic, partitions }) => ({
+            topic,
+            partitions: partitions
+              .filter(({ partition }) =>
+                metadata[topic].map(({ partitionId }) => partitionId).includes(partition)
+              )
+              .map(({ partition }) => ({
+                partition,
+                offset: `${parseInt(partition)}`,
+              })),
+          })),
+        }),
+      }),
+    }
+    assignPartitions = createLagBasedAssigner({
+      cluster,
+      groupId: 'test-topic-group-id',
+      logger: newLogger(),
+    })
+  })
+
+  test('assign partitions evenly based on offset lag', async () => {
+    topics = ['topic-A', 'topic-B']
+    metadata['topic-A'] = Array(14)
+      .fill()
+      .map((_, i) => ({ partitionId: i }))
+
+    metadata['topic-B'] = Array(5)
+      .fill()
+      .map((_, i) => ({ partitionId: i }))
+
+    const members = [
+      { memberId: 'member-3' },
+      { memberId: 'member-1' },
+      { memberId: 'member-4' },
+      { memberId: 'member-2' },
+    ]
+
+    await expect(assignPartitions({ members, topics })).resolves.toEqual([
+      {
+        memberId: 'member-4',
+        memberAssignment: {
+          'topic-A': [10, 9, 2, 1],
+        },
+      },
+      {
+        memberId: 'member-3',
+        memberAssignment: {
+          'topic-A': [11, 8, 3, 0],
+          'topic-B': [0],
+        },
+      },
+      {
+        memberId: 'member-2',
+        memberAssignment: {
+          'topic-A': [12, 7, 4],
+          'topic-B': [4, 1],
+        },
+      },
+      {
+        memberId: 'member-1',
+        memberAssignment: {
+          'topic-A': [13, 6, 5],
+          'topic-B': [3, 2],
+        },
+      },
+    ])
+  })
+})
