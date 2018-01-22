f7e970cb7a00057ba1e38522c263bba47b602570
tulios <ornelas.tulio@gmail.com>
2018-01-22 23:10:10 +0100

    Increase number of retries in test mode

--- src/retry/defaults.test.js
+++ src/retry/defaults.test.js
@@ -2,6 +2,6 @@ module.exports = {
   maxRetryTime: 1000,
   initialRetryTime: 10,
   factor: 0.002, // randomization factor
-  multiplier: 1, // exponential factor
-  retries: 5, // max retries
+  multiplier: 1.2, // exponential factor
+  retries: 15, // max retries
 }
