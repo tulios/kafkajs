35f14c2ae45e0d2ea9e6b0c5fa855dc431b1d8af
tulios <ornelas.tulio@gmail.com>
2018-05-22 00:05:18 +0200

    Add docker-compose file with kafka 0.11

--- scripts/dockerComposeUp.sh
+++ scripts/dockerComposeUp.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 export HOST_IP=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
-docker-compose up --force-recreate -d
+
+COMPOSE_FILE=${COMPOSE_FILE:="docker-compose.yml"}
+
+docker-compose -f "${COMPOSE_FILE}" up --force-recreate -d
 if [ -z ${NO_LOGS} ]; then
   docker-compose logs -f
 fi
