26f589e3736110f234d87410cd8877efe58e9120
tulios <ornelas.tulio@gmail.com>
2018-06-07 23:22:26 +0200

    Add protocol create topics v1

--- src/protocol/requests/createTopics/index.js
+++ src/protocol/requests/createTopics/index.js
@@ -4,6 +4,11 @@ const versions = {
     const response = require('./v0/response')
     return { request: request({ topics, timeout }), response }
   },
+  1: ({ topics, validateOnly, timeout }) => {
+    const request = require('./v1/request')
+    const response = require('./v1/response')
+    return { request: request({ topics, validateOnly, timeout }), response }
+  },
 }
 
 module.exports = {
