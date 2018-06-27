ab0ae4b1762944b3c8f8bccf49251ac0bc5d04d1
tulios <ornelas.tulio@gmail.com>
2018-06-27 10:00:00 +0200

    Add lock primitive

---
+++ src/utils/lock.js
@@ -0,0 +1,40 @@
+const EventEmitter = require('events')
+const { KafkaJSError } = require('../errors')
+
+module.exports = class Lock {
+  constructor({ timeout = 1000 } = {}) {
+    this.locked = false
+    this.timeout = timeout
+    this.emitter = new EventEmitter()
+  }
+
+  async acquire() {
+    return new Promise((resolve, reject) => {
+      if (!this.locked) {
+        this.locked = true
+        return resolve()
+      }
+
+      let timeoutId
+      const tryToAcquire = () => {
+        if (!this.locked) {
+          this.locked = true
+          clearTimeout(timeoutId)
+          this.emitter.removeListener('releaseLock', tryToAcquire)
+          return resolve()
+        }
+      }
+
+      this.emitter.on('releaseLock', tryToAcquire)
+      timeoutId = setTimeout(
+        () => reject(new KafkaJSError('Timeout while acquiring lock')),
+        this.timeout
+      )
+    })
+  }
+
+  async release() {
+    this.locked = false
+    setImmediate(() => this.emitter.emit('releaseLock'))
+  }
+}
