18544618d9129b0cf7ca733bc539568493d0146e
tulios <ornelas.tulio@gmail.com>
2018-08-10 15:39:31 +0200

    Pull Kafka 0.11 image before running tests on travis-ci

--- scripts/dockerComposeUp.sh
+++ scripts/dockerComposeUp.sh
@@ -2,7 +2,7 @@
 
 export HOST_IP=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 
-COMPOSE_FILE=${COMPOSE_FILE:="docker-compose.yml"}
+COMPOSE_FILE=${COMPOSE_FILE:="docker-compose.0_10.yml"}
 
 echo "Running compose file: ${COMPOSE_FILE}:"
 docker-compose -f "${COMPOSE_FILE}" up --force-recreate -d
