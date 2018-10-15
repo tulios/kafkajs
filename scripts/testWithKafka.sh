3ef9d9a4bf6fadd9f351c1267d4836294b0eedd0
Tommy Brunn <tommy.brunn@gmail.com>
2018-10-15 09:30:46 +0200

    Use new environment variable in scripts

--- scripts/testWithKafka.sh
+++ scripts/testWithKafka.sh
@@ -5,7 +5,7 @@ extraArgs="$2"
 
 export HOST_IP=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 export COMPOSE_FILE=${COMPOSE_FILE:="docker-compose.0_11.yml"}
-export DEBUG_LOG_BUFFERS=${DEBUG_LOG_BUFFERS:=1}
+export KAFKAJS_DEBUG_PROTOCOL_BUFFERS=${KAFKAJS_DEBUG_PROTOCOL_BUFFERS:=1}
 
 find_container_id() {
   echo $(docker ps \
