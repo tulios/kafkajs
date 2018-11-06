7b4cb22b599544f8fb1c8259dc95c0a8794a14fc
Ian Walker-Sperber <ianwsperber@gmail.com>
2018-11-06 10:09:32 -0800

    #169 Unit tests for TxnOffsetCommit protocol

---
+++ src/protocol/requests/txnOffsetCommit/v0/request.spec.js
@@ -0,0 +1,20 @@
+const RequestV0Protocol = require('./request')
+
+describe('Protocol > Requests > TxnOffsetCommit > v0', () => {
+  test('request', async () => {
+    const { buffer } = await RequestV0Protocol({
+      transactionalId: 'test-transactional-id',
+      groupId: 'test-group-id',
+      producerId: 20000,
+      producerEpoch: 0,
+      topics: [
+        {
+          topic: 'test-topic',
+          partitions: [{ partition: 1, offset: 0 }, { partition: 2, offset: 0 }],
+        },
+      ],
+    }).encode()
+
+    expect(buffer).toEqual(Buffer.from(require('../fixtures/v0_request.json')))
+  })
+})
