bd321cdb9ed8c43efcf1336d73cff5093ece5fde
Ian Walker-Sperber <ianwsperber@gmail.com>
2018-11-12 15:42:59 -0800

    EoS #200 Document transaction manager

--- src/producer/transactionManager.js
+++ src/producer/transactionManager.js
@@ -1,39 +1,33 @@
 const NO_PRODUCER_ID = -1
-const NO_PRODUCER_EPOCH = 0
 const SEQUENCE_START = 0
 
+/**
+ * Manage behavior for an idempotent producer and transactions.
+ */
 module.exports = ({ logger, cluster }) => {
+  /**
+   * Current producer ID
+   */
   let producerId = NO_PRODUCER_ID
-  let producerEpoch = NO_PRODUCER_EPOCH
-  let producerSequence = {} // Track sequence by topic-partition
+  /**
+   * Current producer epoch
+   */
+  let producerEpoch = 0
+  /**
+   * Idempotent production requires that the producer track the sequence number of messages.
+   *
+   * Sequences are sent with every Record Batch and tracked per Topic-Partition
+   */
+  let producerSequence = {}
 
-  const isInitialized = () => producerId !== NO_PRODUCER_ID || producerEpoch !== NO_PRODUCER_EPOCH
+  const isInitialized = () => producerId !== NO_PRODUCER_ID
 
   return {
-    getProducerId() {
-      return producerId
-    },
-
-    getProducerEpoch() {
-      return producerEpoch
-    },
-
-    getSequence(topic, partition) {
-      if (!isInitialized()) return SEQUENCE_START
-
-      producerSequence[topic] = producerSequence[topic] || {}
-      producerSequence[topic][partition] = producerSequence[topic][partition] || SEQUENCE_START
-
-      return producerSequence[topic][partition]
-    },
-
-    updateSequence(topic, partition, sequence) {
-      if (!isInitialized()) return
-
-      producerSequence[topic] = producerSequence[topic] || {}
-      producerSequence[topic][partition] = sequence
-    },
-
+    /**
+     * Initialize the idempotent producer by making an `InitProducerId` request.
+     *
+     * Overwrites any existing state in this transaction manager
+     */
     initProducerId: async () => {
       await cluster.refreshMetadata()
       // If non-transactional we can request the PID from any broker
@@ -47,6 +41,52 @@ module.exports = ({ logger, cluster }) => {
       producerSequence = {}
 
       logger.debug('Initialized producer id & epoch', { producerId, producerEpoch })
+    },
+
+    /**
+     * Get the current sequence for a given Topic-Partition. Defaults to 0.
+     * @param {string} topic
+     * @param {string} partition
+     * @returns {number}
+     */
+    getSequence(topic, partition) {
+      if (!isInitialized()) return SEQUENCE_START
+
+      producerSequence[topic] = producerSequence[topic] || {}
+      producerSequence[topic][partition] = producerSequence[topic][partition] || SEQUENCE_START
+
+      return producerSequence[topic][partition]
+    },
+
+    /**
+     * Update the sequence for a given Topic-Partition.
+     *
+     * Do nothing if not yet initialized (not idempotent)
+     * @param {string} topic
+     * @param {string} partition
+     * @param {number} sequence
+     */
+    updateSequence(topic, partition, sequence) {
+      if (!isInitialized()) return
+
+      producerSequence[topic] = producerSequence[topic] || {}
+      producerSequence[topic][partition] = sequence
+    },
+
+    /**
+     * Get the current producer id
+     * @returns {number}
+     */
+    getProducerId() {
+      return producerId
+    },
+
+    /**
+     * Get the current producer epoch
+     * @returns {number}
+     */
+    getProducerEpoch() {
+      return producerEpoch
     },
   }
 }
