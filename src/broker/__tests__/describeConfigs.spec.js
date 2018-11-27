aaddb5d6f3a0f1e2fd7c475773c8f60ca3aaa032
tulios <ornelas.tulio@gmail.com>
2018-11-27 20:40:54 +0100

    Make test compatible with both versions (0.11 and 1.1)

--- src/broker/__tests__/describeConfigs.spec.js
+++ src/broker/__tests__/describeConfigs.spec.js
@@ -2,6 +2,8 @@ const { createConnection, connectionOpts, secureRandom, newLogger } = require('t
 const RESOURCE_TYPES = require('../../protocol/resourceTypes')
 const Broker = require('../index')
 
+const sortByConfigName = array => array.sort((a, b) => a.configName.localeCompare(b.configName))
+
 describe('Broker > describeConfigs', () => {
   let seedBroker, broker
 
@@ -94,187 +96,195 @@ describe('Broker > describeConfigs', () => {
         ],
       })
 
-      expect(response).toEqual({
-        resources: [
-          {
-            configEntries: [
-              {
-                configName: 'compression.type',
-                configValue: 'producer',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'message.format.version',
-                configValue: '0.11.0-IV2',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'file.delete.delay.ms',
-                configValue: '60000',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'leader.replication.throttled.replicas',
-                configValue: '',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'max.message.bytes',
-                configValue: '1000012',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'min.compaction.lag.ms',
-                configValue: '0',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'message.timestamp.type',
-                configValue: 'CreateTime',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'min.insync.replicas',
-                configValue: '1',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'segment.jitter.ms',
-                configValue: '0',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'preallocate',
-                configValue: 'false',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'index.interval.bytes',
-                configValue: '4096',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'min.cleanable.dirty.ratio',
-                configValue: '0.5',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'unclean.leader.election.enable',
-                configValue: 'false',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'retention.bytes',
-                configValue: '-1',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'delete.retention.ms',
-                configValue: '86400000',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'cleanup.policy',
-                configValue: 'delete',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'flush.ms',
-                configValue: '9223372036854775807',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'follower.replication.throttled.replicas',
-                configValue: '',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'segment.bytes',
-                configValue: '1073741824',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'retention.ms',
-                configValue: '604800000',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'segment.ms',
-                configValue: '604800000',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'message.timestamp.difference.max.ms',
-                configValue: '9223372036854775807',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'flush.messages',
-                configValue: '9223372036854775807',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-              {
-                configName: 'segment.index.bytes',
-                configValue: '10485760',
-                isDefault: true,
-                isSensitive: false,
-                readOnly: false,
-              },
-            ],
-            errorCode: 0,
-            errorMessage: null,
-            resourceName: topicName1,
-            resourceType: RESOURCE_TYPES.TOPIC,
-          },
-        ],
-        throttleTime: 0,
-      })
+      const expectedConfigEntries = sortByConfigName([
+        {
+          configName: 'compression.type',
+          configValue: 'producer',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'message.format.version',
+          configValue: expect.stringMatching(/^(0\.11\.0-IV2|1\.1-IV0)$/),
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'file.delete.delay.ms',
+          configValue: '60000',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'leader.replication.throttled.replicas',
+          configValue: '',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'max.message.bytes',
+          configValue: '1000012',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'min.compaction.lag.ms',
+          configValue: '0',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'message.timestamp.type',
+          configValue: 'CreateTime',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'min.insync.replicas',
+          configValue: '1',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'segment.jitter.ms',
+          configValue: '0',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'preallocate',
+          configValue: 'false',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'index.interval.bytes',
+          configValue: '4096',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'min.cleanable.dirty.ratio',
+          configValue: '0.5',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'unclean.leader.election.enable',
+          configValue: 'false',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'retention.bytes',
+          configValue: '-1',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'delete.retention.ms',
+          configValue: '86400000',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'cleanup.policy',
+          configValue: 'delete',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'flush.ms',
+          configValue: '9223372036854775807',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'follower.replication.throttled.replicas',
+          configValue: '',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'segment.bytes',
+          configValue: '1073741824',
+          isDefault: expect.any(Boolean),
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'retention.ms',
+          configValue: '604800000',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'segment.ms',
+          configValue: '604800000',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'message.timestamp.difference.max.ms',
+          configValue: '9223372036854775807',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'flush.messages',
+          configValue: '9223372036854775807',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+        {
+          configName: 'segment.index.bytes',
+          configValue: '10485760',
+          isDefault: true,
+          isSensitive: false,
+          readOnly: false,
+        },
+      ])
+
+      expect(response).toEqual(
+        expect.objectContaining({
+          resources: [
+            expect.objectContaining({
+              configEntries: expect.any(Array),
+              errorCode: 0,
+              errorMessage: null,
+              resourceName: topicName1,
+              resourceType: RESOURCE_TYPES.TOPIC,
+            }),
+          ],
+          throttleTime: 0,
+        })
+      )
+
+      expect(sortByConfigName(response.resources[0].configEntries)).toEqual(
+        expect.arrayContaining(expectedConfigEntries)
+      )
     })
   })
 })
