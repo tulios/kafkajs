94f8fda68949c417f502ce7eaa61f39d129b8223
tulios <ornelas.tulio@gmail.com>
2019-01-16 15:08:14 +0100

    Expose network request queue size instrumentation event on the producer

--- src/producer/instrumentationEvents.js
+++ src/producer/instrumentationEvents.js
@@ -8,11 +8,13 @@ const events = {
   DISCONNECT: producerType('disconnect'),
   REQUEST: producerType(networkEvents.NETWORK_REQUEST),
   REQUEST_TIMEOUT: producerType(networkEvents.NETWORK_REQUEST_TIMEOUT),
+  REQUEST_QUEUE_SIZE: producerType(networkEvents.NETWORK_REQUEST_QUEUE_SIZE),
 }
 
 const wrappedEvents = {
   [events.REQUEST]: networkEvents.NETWORK_REQUEST,
   [events.REQUEST_TIMEOUT]: networkEvents.NETWORK_REQUEST_TIMEOUT,
+  [events.REQUEST_QUEUE_SIZE]: networkEvents.NETWORK_REQUEST_QUEUE_SIZE,
 }
 
 const reversedWrappedEvents = swapObject(wrappedEvents)
