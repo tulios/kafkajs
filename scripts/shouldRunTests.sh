fcb99b604314cfabce53343050f4cd7b88358f62
tulios <ornelas.tulio@gmail.com>
2019-03-28 20:27:42 +0100

    Fix skip script and include npm publish task

--- scripts/shouldRunTests.sh
+++ scripts/shouldRunTests.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-set -e
-
-CHANGED_FILES=`git diff --name-only master...$(Build.SourceVersion)`
+CHANGED_FILES=`git diff --name-only master...COMMIT`
 
 [[ -z $CHANGED_FILES ]] && exit 1
 
 for CHANGED_FILE in $CHANGED_FILES; do
-  if ! [[ "$CHANGED_FILE" =~ .md$ ]] && ! [[ "$CHANGED_FILE" =~ ^(docs|website)/.*$ ]]; then
+  if ! [[ "$CHANGED_FILE" =~ .md$ ]] && ! [[ "$CHANGED_FILE" =~ .png$ ]] && ! [[ "$CHANGED_FILE" =~ ^(docs|website|logo)/.*$ ]]; then
     exit 1
   fi
 done
