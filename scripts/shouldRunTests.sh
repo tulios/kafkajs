6185d195f45d78722c2b8770ab7dbaf4c95d6853
tulios <ornelas.tulio@gmail.com>
2019-03-28 20:51:25 +0100

    Clean up skip tests script

--- scripts/shouldRunTests.sh
+++ scripts/shouldRunTests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -ex
 
-CHANGED_FILES=`git diff --name-only master...COMMIT`
+LAST_COMMIT=$(git log --format="%H" -n 1)
+CHANGED_FILES=`git diff --name-only master...${LAST_COMMIT}`
 
+echo -d "LAST_COMMIT=${LAST_COMMIT}"
 [[ -z $CHANGED_FILES ]] && exit 1
+
+echo 'Changed files:'
+echo -d $CHANGED_FILES | tr ' ' '\n'
 
 for CHANGED_FILE in $CHANGED_FILES; do
   if ! [[ "$CHANGED_FILE" =~ .md$ ]] && ! [[ "$CHANGED_FILE" =~ .png$ ]] && ! [[ "$CHANGED_FILE" =~ ^(docs|website|logo)/.*$ ]]; then
