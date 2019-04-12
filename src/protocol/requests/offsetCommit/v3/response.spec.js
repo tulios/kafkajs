a051abeec3142b0397c12167fa66b5f9cd85e820
tulios <ornelas.tulio@gmail.com>
2019-04-12 16:53:14 +0200

    Add protocol offsetCommit v3

---
+++ src/protocol/requests/offsetCommit/v3/response.spec.js
@@ -0,0 +1,18 @@
+const { decode, parse } = require('./response')
+
+describe('Protocol > Requests > OffsetCommit > v3', () => {
+  test('response', async () => {
+    const data = await decode(Buffer.from(require('../fixtures/v3_response.json')))
+    expect(data).toEqual({
+      throttleTime: 0,
+      responses: [
+        {
+          topic: 'test-topic-5c24efe0ac41b91bee85-9985-841d6145-c897-4471-bd09-acd8b4c905f2',
+          partitions: [{ partition: 0, errorCode: 0 }],
+        },
+      ],
+    })
+
+    await expect(parse(data)).resolves.toBeTruthy()
+  })
+})
