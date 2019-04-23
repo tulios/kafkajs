86fb7c6651f12898cc88838aeebaf398cd458e2a
Tommy Brunn <tommy.brunn@gmail.com>
2019-04-23 14:31:03 +0200

    Add metadata protocol decode test with offline replica

--- src/protocol/requests/metadata/v5/response.spec.js
+++ src/protocol/requests/metadata/v5/response.spec.js
@@ -48,4 +48,104 @@ describe('Protocol > Requests > Metadata > v5', () => {
 
     await expect(parse(data)).resolves.toBeTruthy()
   })
+
+  test('response with offline replicas', async () => {
+    const data = await decode(Buffer.from(require('../fixtures/v5_offline_replicas_response.json')))
+
+    expect(data).toEqual({
+      throttleTime: 0,
+      brokers: [
+        {
+          nodeId: 1,
+          host: '10.3.223.121',
+          port: 9095,
+          rack: null,
+        },
+        {
+          nodeId: 0,
+          host: '10.3.223.121',
+          port: 9092,
+          rack: null,
+        },
+      ],
+      clusterId: 'tDr4DgFsS96sOEZu6e-N-Q',
+      controllerId: 1,
+      topicMetadata: [
+        {
+          topicErrorCode: 0,
+          topic: 'topic-test',
+          isInternal: false,
+          partitionMetadata: [
+            {
+              isr: [],
+              leader: -1,
+              offlineReplicas: [2],
+              partitionErrorCode: 5,
+              partitionId: 2,
+              replicas: [2],
+            },
+            {
+              isr: [],
+              leader: -1,
+              offlineReplicas: [2],
+              partitionErrorCode: 5,
+              partitionId: 5,
+              replicas: [2],
+            },
+            {
+              isr: [1],
+              leader: 1,
+              offlineReplicas: [],
+              partitionErrorCode: 0,
+              partitionId: 4,
+              replicas: [1],
+            },
+            {
+              isr: [1],
+              leader: 1,
+              offlineReplicas: [],
+              partitionErrorCode: 0,
+              partitionId: 1,
+              replicas: [1],
+            },
+            {
+              isr: [0],
+              leader: 0,
+              offlineReplicas: [],
+              partitionErrorCode: 0,
+              partitionId: 3,
+              replicas: [0],
+            },
+            {
+              isr: [0],
+              leader: 0,
+              offlineReplicas: [],
+              partitionErrorCode: 0,
+              partitionId: 0,
+              replicas: [0],
+            },
+          ],
+        },
+        {
+          isInternal: false,
+          partitionMetadata: [
+            {
+              isr: [],
+              leader: -1,
+              offlineReplicas: [2],
+              partitionErrorCode: 5,
+              partitionId: 0,
+              replicas: [2],
+            },
+          ],
+          topic: 'test-topic-tommy',
+          topicErrorCode: 0,
+        },
+      ],
+    })
+
+    await expect(parse(data)).rejects.toThrow(
+      'There is no leader for this topic-partition as we are in the middle of a leadership election'
+    )
+  })
 })
