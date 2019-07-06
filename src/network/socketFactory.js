506e85716467e928d24c2f0da9334d5aa6542cee
Carlos Bajo <carlosabpreciado@gmail.com>
2019-07-06 15:10:47 -0500

    Auto eslint configuration error

--- src/network/socketFactory.js
+++ src/network/socketFactory.js
@@ -8,7 +8,6 @@ module.exports = () => {
     const socket = ssl
       ? tls.connect(Object.assign({ host, port }, ssl), onConnect)
       : net.connect({ host, port }, onConnect)
-
     socket.setKeepAlive(true, KEEP_ALIVE_DELAY)
 
     return socket
