4be75e90bfe0c08feec51a6b189ceab3b92ae5c0
Carlos Bajo <carlosabpreciado@gmail.com>
2019-07-06 23:18:39 -0500

    Testlint error

--- src/network/socketFactory.js
+++ src/network/socketFactory.js
@@ -8,6 +8,7 @@ module.exports = () => {
     const socket = ssl
       ? tls.connect(Object.assign({ host, port }, ssl), onConnect)
       : net.connect({ host, port }, onConnect)
+
     socket.setKeepAlive(true, KEEP_ALIVE_DELAY)
 
     return socket
