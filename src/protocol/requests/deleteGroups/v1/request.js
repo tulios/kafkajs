3b563d526823b8f435e27f7854b3f4820e049d4c
Dario Filković <dario.filkovic@leapbit.com>
2020-02-24 20:24:09 +0100

    API version fix

--- src/protocol/requests/deleteGroups/v1/request.js
+++ src/protocol/requests/deleteGroups/v1/request.js
@@ -1,7 +1,7 @@
 const requestV0 = require('../v0/request')
 
 /**
- * ListGroups Request (Version: 1)
+ * DeleteGroups Request (Version: 1)
  */
 
 module.exports = groupIds => Object.assign(requestV0(groupIds), { apiVersion: 1 })
