ee96f8285d8739bbcd1563dcfe7c95a73972e21f
galesky <leonardo.galesky@gmail.com>
2020-04-19 13:51:30 -0300

    fix(test): fixed username shadowing in test scram setup

--- scripts/createScramCredentials.sh
+++ scripts/createScramCredentials.sh
@@ -9,10 +9,15 @@ find_container_id() {
     -q)
 }
 
-USERNAME=${USERNAME:='testscram'}
+ZOOKEEPER_USERNAME=${ZOOKEEPER_USERNAME:='testscram'}
 PASSWORD_256=${PASSWORD_256:='testtestscram=256'}
 PASSWORD_512=${PASSWORD_512:='testtestscram=512'}
 
 docker exec \
   $(find_container_id) \
-  bash -c "kafka-configs --zookeeper zookeeper:2181 --alter --add-config 'SCRAM-SHA-256=[iterations=8192,password=${PASSWORD_256}],SCRAM-SHA-512=[password=${PASSWORD_512}]' --entity-type users --entity-name ${USERNAME}"
+  bash -c "kafka-configs --zookeeper zookeeper:2181 --alter --add-config 'SCRAM-SHA-256=[iterations=8192,password=${PASSWORD_256}],SCRAM-SHA-512=[password=${PASSWORD_512}]' --entity-type users --entity-name ${ZOOKEEPER_USERNAME}"
+
+echo "The credentials have been setup as following"
+echo "ZOOKEEPER_USERNAME: ${ZOOKEEPER_USERNAME}"
+echo "PASSWORD_256: ${PASSWORD_256}"
+echo "PASSWORD_512: ${PASSWORD_512}"
