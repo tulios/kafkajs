5d70cc526a31c145e5d6d53be7d6df80b0225f35
Andreas Kohn <andreas.kohn@gmail.com>
2020-06-01 11:36:08 +0200

    Fix typo in variable name

--- src/consumer/__tests__/runner.spec.js
+++ src/consumer/__tests__/runner.spec.js
@@ -208,10 +208,10 @@ describe('Consumer > Runner', () => {
   })
 
   it('calls onCrash for any other errors', async () => {
-    const unknowError = new KafkaJSProtocolError(createErrorFromCode(UNKNOWN))
+    const unknownError = new KafkaJSProtocolError(createErrorFromCode(UNKNOWN))
     consumerGroup.join
       .mockImplementationOnce(() => {
-        throw unknowError
+        throw unknownError
       })
       .mockImplementationOnce(() => true)
 
@@ -223,7 +223,7 @@ describe('Consumer > Runner', () => {
     await sleep(100)
 
     expect(runner.scheduleFetch).not.toHaveBeenCalled()
-    expect(onCrash).toHaveBeenCalledWith(unknowError)
+    expect(onCrash).toHaveBeenCalledWith(unknownError)
   })
 
   it('crashes on KafkaJSNotImplemented errors', async () => {
@@ -333,9 +333,9 @@ describe('Consumer > Runner', () => {
     })
 
     it('a triggered rejoin failing should cause a crash', async () => {
-      const unknowError = new KafkaJSProtocolError(createErrorFromCode(UNKNOWN))
+      const unknownError = new KafkaJSProtocolError(createErrorFromCode(UNKNOWN))
       consumerGroup.join.mockImplementationOnce(() => {
-        throw unknowError
+        throw unknownError
       })
       consumerGroup.commitOffsets.mockImplementationOnce(() => {
         throw rebalancingError()
@@ -345,7 +345,7 @@ describe('Consumer > Runner', () => {
 
       await sleep(100)
 
-      expect(onCrash).toHaveBeenCalledWith(unknowError)
+      expect(onCrash).toHaveBeenCalledWith(unknownError)
     })
   })
 })
