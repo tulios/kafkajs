aad52147754106ebc363ff357f7996238ce423b0
Tommy Brunn <tommy.brunn@gmail.com>
2020-06-03 15:35:49 +0200

    Make Array shuffle test pass in Node >= 11.0.0
    Due to a change in how array sorting works, the shuffle test would fail in Node >= 11.0.0.
    Instead of relying on implementation details of how the engine implements sorting, we'll
    add a requirement on the shuffle implementation to always return a different order than
    the input array. That way we can simply assert that the returned array is different
    from the input, rather than asserting on the exact order, which may differ between engine versions.

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,1 +1,19 @@
-module.exports = array => [...array].sort(() => Math.random() - 0.5)
+const shuffle = array => array.sort(() => Math.random() - 0.5)
+
+module.exports = array => {
+  if (!Array.isArray(array)) {
+    throw new TypeError("'array' is not an array")
+  }
+
+  if (array.length < 2) {
+    return array
+  }
+
+  let shuffled = [...array]
+
+  while (shuffled.every((value, index) => value === array[index])) {
+    shuffled = shuffle(shuffled)
+  }
+
+  return shuffled
+}
