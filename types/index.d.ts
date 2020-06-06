99ee94f599f9787a9b8ee5285e7b7d5ed8e21034
Waleed ASHRAF <waleedashraf@outlook.com>
2020-06-06 20:41:42 +0200

    add missing consumer event in types

--- types/index.d.ts
+++ types/index.d.ts
@@ -542,6 +542,7 @@ export type ConsumerEvents = {
   HEARTBEAT: 'consumer.heartbeat'
   COMMIT_OFFSETS: 'consumer.commit_offsets'
   GROUP_JOIN: 'consumer.group_join'
+  FETCH_START: 'consumer.fetch_start'
   FETCH: 'consumer.fetch'
   START_BATCH_PROCESS: 'consumer.start_batch_process'
   END_BATCH_PROCESS: 'consumer.end_batch_process'
