f4e4fb6d7aec899855c31a1961924bd030afe475
Andreas Kohn <andreas.kohn@gmail.com>
2020-06-23 16:30:27 +0200

    Support Fetch API v8
    Fetch API v8 is identical to v7, but using v8 means that the client understands how
    to use client-side throttling.
    
    See https://cwiki.apache.org/confluence/display/KAFKA/KIP-219+-+Improve+quota+communication

---
+++ src/protocol/requests/fetch/v8/request.spec.js
@@ -0,0 +1,26 @@
+const RequestV8Protocol = require('./request')
+
+describe('Protocol > Requests > Fetch > v8', () => {
+  test('request', async () => {
+    const minBytes = 1
+    const maxBytes = 10485760 // 10MB
+    const maxWaitTime = 100
+    const maxBytesPerPartition = 1048576 // 1MB
+    const topics = [
+      {
+        topic: 'test-topic-2077b9d2b36c4082e594-4020-b5a52b27-56df-4b87-800d-82c1cf26317d',
+        partitions: [{ partition: 0, fetchOffset: 0, maxBytes: maxBytesPerPartition }],
+      },
+    ]
+
+    const { buffer } = await RequestV8Protocol({
+      replicaId: -1,
+      maxWaitTime,
+      minBytes,
+      maxBytes,
+      topics,
+    }).encode()
+
+    expect(buffer).toEqual(Buffer.from(require('../fixtures/v8_request.json')))
+  })
+})
