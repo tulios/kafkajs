457f62aa906a4c1bdc624d48644a9ada6005eda4
Robert Shepherd <rob.shepherd@navitas.com>
2020-08-11 16:03:17 +0800

    replaced default doc route

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -40,7 +40,7 @@ function Versions(props) {
                   <a
                     href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                       props.language ? props.language + '/' : ''
-                    }doc1`}>
+                    }getting-started`}>
                     Documentation
                   </a>
                 </td>
@@ -65,7 +65,7 @@ function Versions(props) {
                   <a
                     href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                       props.language ? props.language + '/' : ''
-                    }next/doc1`}>
+                    }next/getting-started`}>
                     Documentation
                   </a>
                 </td>
@@ -91,7 +91,7 @@ function Versions(props) {
                         <a
                           href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                             props.language ? props.language + '/' : ''
-                          }${version}/doc1`}>
+                          }${version}/getting-started`}>
                           Documentation
                         </a>
                       </td>
