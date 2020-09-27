609c0853b9104240c7f216c9b03e136066d4833a
Arty <26905074+artysidorenko@users.noreply.github.com>
2020-09-27 15:00:52 +0100

    add unit tests

--- src/admin/__tests__/fetchOffsets.spec.js
+++ src/admin/__tests__/fetchOffsets.spec.js
@@ -1,8 +1,19 @@
 const createAdmin = require('../index')
-const { secureRandom, createCluster, newLogger, createTopic } = require('testHelpers')
+const createProducer = require('../../producer')
+const createConsumer = require('../../consumer')
+const {
+  secureRandom,
+  createCluster,
+  newLogger,
+  createTopic,
+  createModPartitioner,
+  waitForConsumerToJoinGroup,
+  generateMessages,
+  waitForMessages,
+} = require('testHelpers')
 
 describe('Admin', () => {
-  let admin, cluster, consumer, groupId, logger, topicName
+  let admin, cluster, groupId, logger, topicName
 
   beforeEach(async () => {
     topicName = `test-topic-${secureRandom()}`
@@ -19,7 +30,6 @@ describe('Admin', () => {
 
   afterEach(async () => {
     admin && (await admin.disconnect())
-    consumer && (await consumer.disconnect())
   })
 
   describe('fetchOffsets', () => {
@@ -59,6 +69,90 @@ describe('Admin', () => {
       })
 
       expect(offsets).toEqual([{ partition: 0, offset: '13', metadata: null }])
+    })
+
+    describe('when resolveOffsets option is enabled', () => {
+      let producer, consumer, messagesConsumed
+
+      beforeEach(async () => {
+        producer = createProducer({
+          cluster,
+          createPartitioner: createModPartitioner,
+          logger,
+        })
+        await producer.connect()
+
+        consumer = createConsumer({
+          cluster,
+          groupId,
+          maxWaitTimeInMs: 100,
+          logger,
+        })
+        await consumer.connect()
+        await consumer.subscribe({ topic: topicName, fromBeginning: true })
+
+        messagesConsumed = []
+        consumer.run({ eachMessage: async event => messagesConsumed.push(event) })
+        await waitForConsumerToJoinGroup(consumer)
+
+        const messages = generateMessages({ number: 10 })
+        await producer.send({
+          acks: 1,
+          topic: topicName,
+          messages,
+        })
+        await waitForMessages(messagesConsumed, { number: messages.length })
+        await consumer.stop()
+      })
+
+      afterEach(async () => {
+        producer && (await producer.disconnect())
+        consumer && (await consumer.disconnect())
+      })
+
+      test('returns latest topic offsets after resolving, and persists them', async () => {
+        await admin.resetOffsets({ groupId, topic: topicName })
+
+        const offsetsBeforeResolving = await admin.fetchOffsets({
+          groupId,
+          topic: topicName,
+        })
+        const offsetsUponResolving = await admin.fetchOffsets({
+          groupId,
+          topic: topicName,
+          resolveOffsets: true,
+        })
+        const offsetsAfterResolving = await admin.fetchOffsets({
+          groupId,
+          topic: topicName,
+        })
+
+        expect(offsetsBeforeResolving).toEqual([{ partition: 0, offset: '-1', metadata: null }])
+        expect(offsetsUponResolving).toEqual([{ partition: 0, offset: '10', metadata: null }])
+        expect(offsetsAfterResolving).toEqual([{ partition: 0, offset: '10', metadata: null }])
+      })
+
+      test('returns earliest topic offsets after resolving, and persists them', async () => {
+        await admin.resetOffsets({ groupId, topic: topicName, earliest: true })
+
+        const offsetsBeforeResolving = await admin.fetchOffsets({
+          groupId,
+          topic: topicName,
+        })
+        const offsetsUponResolving = await admin.fetchOffsets({
+          groupId,
+          topic: topicName,
+          resolveOffsets: true,
+        })
+        const offsetsAfterResolving = await admin.fetchOffsets({
+          groupId,
+          topic: topicName,
+        })
+
+        expect(offsetsBeforeResolving).toEqual([{ partition: 0, offset: '-2', metadata: null }])
+        expect(offsetsUponResolving).toEqual([{ partition: 0, offset: '0', metadata: null }])
+        expect(offsetsAfterResolving).toEqual([{ partition: 0, offset: '0', metadata: null }])
+      })
     })
   })
 })
