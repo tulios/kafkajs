10383810932889e3a18e3b90ed485572981ba3b3
Tommy Brunn <tommy.brunn@gmail.com>
2020-11-17 15:10:08 +0100

    Revert "Merge pull request #901 from tulios/fix-contact-form"
    This reverts commit 02bc8a3c19504d182df131914ee9af4661bf060c, reversing
    changes made to e72263f493ffda7ee45a7c149b478161d2766727.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,7 +10,7 @@
 
 const repoUrl = 'https://github.com/tulios/kafkajs'
 const slackUrl = 'https://kafkajs-slackin.herokuapp.com/'
-const contactFormUrl = 'https://formcarry.com/s/ifZUgcBlj'
+const contactFormUrl = process.env.CONTACT_FORM_URL
 
 const siteConfig = {
   title: 'KafkaJS',
