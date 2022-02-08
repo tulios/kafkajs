3629557fca52a63fd8d530fc543ff804808505db
Tommy Brunn <tommy.brunn@gmail.com>
2022-02-08 09:32:54 +0100

    Validate brokers
    Fixes #1229

--- src/cluster/connectionBuilder.js
+++ src/cluster/connectionBuilder.js
@@ -37,36 +37,49 @@ module.exports = ({
 }) => {
   let index = 0
 
-  const getBrokers = async () => {
+  const isValidBroker = broker => {
+    return broker && typeof broker === 'string' && broker.length > 0
+  }
+
+  const validateBrokers = brokers => {
     if (!brokers) {
-      throw new KafkaJSNonRetriableError(`Failed to connect: brokers parameter should not be null`)
+      throw new KafkaJSNonRetriableError(`Failed to connect: brokers should not be null`)
     }
 
-    // static list
     if (Array.isArray(brokers)) {
       if (!brokers.length) {
         throw new KafkaJSNonRetriableError(`Failed to connect: brokers array is empty`)
       }
-      return brokers
-    }
 
-    // dynamic brokers
+      brokers.forEach((broker, index) => {
+        if (!isValidBroker(broker)) {
+          throw new KafkaJSNonRetriableError(
+            `Failed to connect: broker at index ${index} is invalid "${typeof broker}"`
+          )
+        }
+      })
+    }
+  }
+
+  const getBrokers = async () => {
     let list
-    try {
-      list = await brokers()
-    } catch (e) {
-      const wrappedError = new KafkaJSConnectionError(
-        `Failed to connect: "config.brokers" threw: ${e.message}`
-      )
-      wrappedError.stack = `${wrappedError.name}\n  Caused by: ${e.stack}`
-      throw wrappedError
+
+    if (typeof brokers === 'function') {
+      try {
+        list = await brokers()
+      } catch (e) {
+        const wrappedError = new KafkaJSConnectionError(
+          `Failed to connect: "config.brokers" threw: ${e.message}`
+        )
+        wrappedError.stack = `${wrappedError.name}\n  Caused by: ${e.stack}`
+        throw wrappedError
+      }
+    } else {
+      list = brokers
     }
 
-    if (!list || list.length === 0) {
-      throw new KafkaJSConnectionError(
-        `Failed to connect: "config.brokers" returned void or empty array`
-      )
-    }
+    validateBrokers(list)
+
     return list
   }
 
