fdde0ab579707909dad6ce95fe12ef2ff317b554
Tommy Brunn <tommy.brunn@gmail.com>
2022-05-05 13:16:29 +0200

    Re-export default partitioner as JavaCompatiblePartitioner for backwards compatibility

--- src/producer/partitioners/index.js
+++ src/producer/partitioners/index.js
@@ -4,4 +4,11 @@ const LegacyPartitioner = require('./legacy')
 module.exports = {
   DefaultPartitioner,
   LegacyPartitioner,
+  /**
+   * @deprecated Use DefaultPartitioner instead
+   *
+   * The JavaCompatiblePartitioner was renamed DefaultPartitioner
+   * and made to be the default in 2.0.0.
+   */
+  JavaCompatiblePartitioner: DefaultPartitioner,
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -96,6 +96,13 @@ export type LegacyPartitioner = ICustomPartitioner
 export const Partitioners: {
   DefaultPartitioner: DefaultPartitioner,
   LegacyPartitioner: LegacyPartitioner,
+  /**
+   * @deprecated Use DefaultPartitioner instead
+   *
+   * The JavaCompatiblePartitioner was renamed DefaultPartitioner
+   * and made to be the default in 2.0.0.
+   */
+  JavaCompatiblePartitioner: DefaultPartitioner
 }
 
 export type PartitionMetadata = {
